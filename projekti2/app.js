dc8afef6ce4540f257de0db6d126ccdcae2d8544
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2024-03-28 13:48:16 +0200

    installations

---
+++ projekti2/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const mongoose = require("mongoose");
+const read = require("projekti2/readMongoDB.js")
+var app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/getall", function (req, res) { })
+
+app.get("/api/:id", function (req, res) { })
+
+app.post("/api/add", function (req, res) { })
+
+app.put("/api/update/:id", function (req, res) { })
+
+app.delete("/api/delete/:id", function (req, res) { })
+
+app.listen(port, function () {
+    console.log("Kuunnellaan porttia" + port + "!");
+});
