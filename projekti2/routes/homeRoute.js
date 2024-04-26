6d01a631fb08b43979af4e6aa97b66c5ab9cd04b
joelmusakka <91203662+joelmusa@users.noreply.github.com>
2024-04-26 13:39:12 +0300

    Made index.html file. getAll works partly for index.html.

--- projekti2/routes/homeRoute.js
+++ projekti2/routes/homeRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express.Router();
+const fs = require("fs");
+const path = require("path");
 
+app.use("FullStack-kurssi_2024_projektit/projekti2", express.static("public"));
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the API!")
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 module.exports = app;
